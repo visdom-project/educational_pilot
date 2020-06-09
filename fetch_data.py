a25e4be9b3edaf56bbe1cc914aba65400637e1af
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2020-06-09 11:43:23 +0300

    Add authentication.

--- fetch_data.py
+++ fetch_data.py
@@ -1,17 +1,20 @@
 import pycurl
 from io import BytesIO
+import sys
 
 
-def make_get_request(url):
+def make_get_request(url, access_token):
+
     bytes_obj = BytesIO()
     curl_obj = pycurl.Curl()
 
     curl_obj.setopt(curl_obj.URL, url)
     curl_obj.setopt(curl_obj.HEADERFUNCTION, display_header)
     curl_obj.setopt(curl_obj.WRITEDATA, bytes_obj)
+    curl_obj.setopt(curl_obj.HTTPHEADER, ['Authorization: Token {:s}'.format(access_token)])
 
     curl_obj.perform()
-
+    
     curl_obj.close()
 
     get_body = bytes_obj.getvalue()
@@ -23,10 +26,27 @@ def display_header(header_line):
     print(header_line)
 
 
+def get_arguments():
+    if len(sys.argv) > 1:
+        target_URL = sys.argv[1]
+    else:
+        print("fetch_data.py: Error: missing argument: target API URL!")
+        return False, False
+
+    if len(sys.argv) > 2:
+        access_token = sys.argv[2]
+    else:
+        print("fetch_data.py: Error: missing argument: Access token for target API!")
+        return False, False
+
+    return target_URL, access_token
+
+
 def main():
-    target_URL = 'https://plus.tuni.fi/api/v2/courses/40/'
-    
-    make_get_request(target_URL)
+    target_URL, access_token = get_arguments()
+
+    if target_URL != False:
+        make_get_request(target_URL, access_token)
 
 
 main()
