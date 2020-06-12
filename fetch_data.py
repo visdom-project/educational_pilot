8488c1900718eba34e9264664a52a1bb64b9eb7f
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2020-06-12 13:11:00 +0300

    Add data fetcher feature: copy data from api points to elasticsearch cluster.

--- fetch_data.py
+++ fetch_data.py
@@ -2,6 +2,7 @@ import pycurl
 from urllib.parse import urlencode
 from io import BytesIO
 import sys
+import json
 
 
 def make_get_request(url, access_token="", headers=[], data=""):
@@ -10,7 +11,7 @@ def make_get_request(url, access_token="", headers=[], data=""):
     curl_obj = pycurl.Curl()
 
     curl_obj.setopt(curl_obj.URL, url)
-    curl_obj.setopt(curl_obj.HEADERFUNCTION, display_header)
+    #curl_obj.setopt(curl_obj.HEADERFUNCTION, display_header)   # Print header to stdout
     curl_obj.setopt(curl_obj.WRITEDATA, bytes_obj)
 
     if len(data) > 1:
@@ -18,7 +19,7 @@ def make_get_request(url, access_token="", headers=[], data=""):
 
     if len(access_token) > 1:
         # Authenticate:
-        headers.append('Authorization: Token {:s}'.format(access_token))
+        headers.append("Authorization: Token {:s}".format(access_token))
     
     curl_obj.setopt(curl_obj.HTTPHEADER, headers)
 
@@ -30,64 +31,94 @@ def make_get_request(url, access_token="", headers=[], data=""):
 
 def make_put_request(url, data, header):
 
-    buffer = BytesIO(data.encode('utf8'))
+    data_buffer = BytesIO(data.encode('utf8'))
+    response_buffer = BytesIO()
 
     curl_obj = pycurl.Curl()
 
     curl_obj.setopt(curl_obj.URL, url)
     curl_obj.setopt(curl_obj.UPLOAD, 1)
-    curl_obj.setopt(curl_obj.READDATA, buffer)
+    curl_obj.setopt(curl_obj.READDATA, data_buffer)
     curl_obj.setopt(curl_obj.HTTPHEADER, header)
+    curl_obj.setopt(curl_obj.WRITEDATA, response_buffer)
 
     curl_obj.perform()
     curl_obj.close()
+
+    return response_buffer.getvalue().decode('utf8')
 
 
 def display_header(header_line):
     header_line = header_line.decode('utf8')
     print(header_line)
+    print()
 
 
-def get_arguments():
-    if len(sys.argv) > 1:
-        target_URL = sys.argv[1]
-    else:
-        print("fetch_data.py: Warning: missing argument: target API URL!")
-        return False, False
+def get_arguments(api_names):
+    target_URLs = {}
+    access_tokens = {}
+    apis = []
+    for api_name in api_names:
+        apis.append([api_name, '', ''])
 
-    if len(sys.argv) > 2:
-        access_token = sys.argv[2]
-    else:
-        print("fetch_data.py: Warning: missing argument: Access token for target API!")
-        return False, False
+    for i in [1, 3]:
+        if len(sys.argv) > i:
+            target_URLs[apis[i-1]] = sys.argv[i]
+        else:
+            print("fetch_data.py: Warning: missing argument: target API URL!")
+            return False, False
 
-    return target_URL, access_token
+    for i in [2, 4]:
+        if len(sys.argv) > i:
+            access_tokens[apis[i-2]] = sys.argv[i]
+        else:
+            print("fetch_data.py: Warning: missing argument: Access token for target API!")
+            return False, False
+
+    return target_URLs, access_tokens
 
 
 def read_secrets():
-    target_URL = False
-    access_token = False
-    secrets = []
+    print("fetch_data.py: Reading API URL and acces key from the secrets file.")
+    secrets = {}
     
     with open("secrets", 'r') as file:
 
         for row in file:
-            if row.startswith("#"):
+            row = row.rstrip()
+            if row.startswith("#") or row == "":
                 continue
 
-            split = row.split()
+            # Example: GITLAB_API_KEY = qwertyuiop1234567
+            row_split = row.split()
+            key_split = row_split[0].lower().split('_') # ['gitlab', 'api', 'key']
+            api_name = key_split[0]                     # 'gitlab'
+            key_name = "API {:s}s".format(key_split[2]) # 'API keys'
 
-            if len(split) == 3:
-                secrets.append(split[2])
-    
-    if len(secrets) == 2:
-        target_URL = secrets[0]
-        access_token = secrets[1]
-        print("fetch_data.py: API URL and access key succesfully read from secrets!\n")
+            if len(key_split) > 3:
+                api_name_specifier = key_split[3]
+            else:
+                api_name_specifier = 'root'
+
+            whole_name = "{:s}".format(api_name)
+            if key_name == "API urls":
+                whole_name = "{:s}-{:s}".format(api_name, api_name_specifier)
+
+            if len(row_split) == 3:
+                if api_name not in secrets.keys():
+                    secrets[api_name] = {}
+
+                if key_name in secrets[api_name]:
+                    secrets[api_name][key_name][whole_name] = row_split[2]
+                else:
+                    secrets[api_name][key_name] = {whole_name: row_split[2]}
+
+    if len(secrets) > 1:
+        print("fetch_data.py: read some API URL and access key succesfully from secrets!\n")
     else:
-        print("fetch_data.py: Error: could not read API url and access key from secrets!")
+        print("fetch_data.py: Error: could not read any API url or access key from secrets!")
     
-    return target_URL, access_token
+    return secrets
 
 
 def get_id():
@@ -111,17 +142,13 @@ def get_id():
     return document_id
 
 
-def write_to_elasticsearch(data):
+def write_to_elasticsearch(data, index='testindex', data_type='_doc', document_id=get_id()):
     header = ['Content-Type: application/json']
-    
-    index = 'testindex'
-    data_type = '_doc'
-    document_id = get_id()
 
     url = 'http://localhost:9200/{:s}/{:s}/{:s}'.format(index, data_type, document_id)
 
     # curl -XPUT 'http://localhost:9200/testindex/_doc/1' -H 'Content-Type: application/json' -d '{"name":"John Doe"}'
-    make_put_request(url, data, header)
+    return make_put_request(url, data, header)
 
 
 def search_elasticsearch(query, index=''):
@@ -131,19 +158,61 @@ def search_elasticsearch(query, index=''):
     make_get_request(url, headers=header)
 
 
-def main():
-    target_URL, access_token = get_arguments()
+def copy_api_point_to_index(url, access_token, api_name):
+    reply_body = make_get_request(url, access_token, [])
+    reply_body_json = json.loads(reply_body)
     
-    if target_URL == False:
-        print("fetch_data.py: Resorting to fallback: reading API URL and acces key from secrets file.")
-        target_URL, access_token = read_secrets()
+    if "error" in reply_body_json:
+        print("error occured in get request from url: {:s}".format(url))
+        print("Reply body:", reply_body)
+        return False
+    
+    if isinstance(reply_body_json, list):
+        for item in reply_body_json:
 
-    if target_URL != False:
-        reply = make_get_request(target_URL, access_token)
-        print("Reply request body:")
-        print(reply)
-        #write_to_elasticsearch('{"name":"John Doe"}')
-        #search_elasticsearch('{"query": {"match": {"message": "login successful"}}}')
+            id = get_id()
+            if "id" in item.keys():
+                id = str(item['id'])
+
+            reply_body = write_to_elasticsearch(json.dumps(item), api_name, '_doc', id)
+            reply_body_json = json.loads(reply_body)
+
+            if "error" in reply_body_json.keys():
+                print("Error reply from Elastic:\n", reply_body)
+                return False
+            else:
+                print("Wrote successfully to ElasticSearch.")
+    else:
+        reply_body = write_to_elasticsearch(reply_body, api_name, '_doc', get_id())
+        reply_body_json = json.loads(reply_body)
+
+        if "error" in reply_body_json.keys():
+            print("Error reply from Elastic:\n", reply_body)
+            return False
+    
+
+def main():
+
+    # Read access tokens, api names and URLs:
+    secrets = read_secrets()
+    print(secrets)
+    
+    # Copy data from APIs to ElasticSearch cluster:
+    for api_name in secrets.keys():
+
+        api_details = secrets[api_name]
+
+        if 'API keys' in api_details:
+            access_token = api_details['API keys'][api_name]
+        else:
+            access_token = ""
+        
+        if 'API urls' in api_details:
+            for api_name in api_details['API urls'].keys():
+                url = api_details['API urls'][api_name]
+                copy_api_point_to_index(url, access_token, api_name)
+
+    #search_elasticsearch('{"query": {"match": {"message": "login successful"}}}')
 
 
 main()
