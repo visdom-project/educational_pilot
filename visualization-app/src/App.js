6c34963bbc6234176ad03c3082a627b11a07f7c7
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2020-06-15 11:15:41 +0300

    Draw a basic line chart.

--- visualization-app/src/App.js
+++ visualization-app/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
 
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const LineChartVisu = ({data}) => {
+  return (
+    <LineChart width={400} height={400} data={data}>
+      <Line class="basic-line" type="monotone" dataKey="uv" />
+      <CartesianGrid class="basic-grid" />
+      <XAxis dataKey="name" />
+      <YAxis />
+    </LineChart>
+  )
+}
+
 function App() {
+
+  const data = [{name: '1', uv: 0, pv: 0, amt: 0},
+                {name: '2', uv: 200, pv: 0, amt: 0},
+                {name: '3', uv: 300, pv: 0, amt: 0},
+                {name: '4', uv: 500, pv: 0, amt: 0}]
+
   return (
     <div className="App">
       <header className="App-header">
-        Muikka!
+        Visu-App
       </header>
+      <LineChartVisu data={data} />
     </div>
   );
 }
