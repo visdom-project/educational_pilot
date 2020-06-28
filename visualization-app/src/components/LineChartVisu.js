05481d522f3a114774b5403ae44ec30a76546f51
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2020-06-28 14:47:27 +0300

    Implement displaying basic progress line chart for weekly points and cumulative points.

--- visualization-app/src/components/LineChartVisu.js
+++ visualization-app/src/components/LineChartVisu.js
@@ -1,22 +1,19 @@
 import React from 'react';
 
-import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-const SubHeader = ({title}) => 
-  <header className="App-subheader">{title}</header>
-
-const LineChartVisu = ({data, title}) => {
+const LineChartVisu = ({data, title, keys, axisNames}) => {
   return (
     <>
-      <SubHeader title={title} />
-      <div className="intended">
-        <LineChart width={400} height={400} data={data}>
-          <Line class="basic-line" type="monotone" dataKey="uv" />
-          <CartesianGrid class="basic-grid" />
-          <XAxis dataKey="name" />
-          <YAxis />
-        </LineChart>
-      </div>
+      <h2>{title}</h2>
+      <LineChart width={document.documentElement.clientWidth*0.9} height={250} data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" label={{ value: axisNames[0], position: 'insideBottomRight' }} />
+        <YAxis label={{ value: axisNames[1], position: 'insideTopLeft', offset: 15 }}/>
+        <Tooltip />
+        {keys.map(key => <Line key={key} type="linear" dataKey={key} stroke="#8884d8" />)}
+      </LineChart>
     </>
   )
 }
--- visualization-app/src/components/ProgressTab.js
+++ visualization-app/src/components/ProgressTab.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import LineChartVisu from './LineChartVisu';
+import dataService from '../services/studentData'
 
 const ProgressTab = () => {
+
+  const [ studentIds, setStudentIds ] = useState([])
+  const [ weeklyPoints, setWeeklyPoints ] = useState([])
+  const [ cumulativeWeeklyPoints, setcumulativeWeeklyPoints ] = useState([])
+  const [ weeks, setWeeks ] = useState([])
+  const [ weeksToShow, setWeeksToShow ] = useState()
+
+  useEffect(
+    () => {
+      setStudentIds(dataService.getStudentIds())
+
+      const weeks = dataService.getWeeks()
+      setWeeks(weeks)
+
+      const formatted = weeks.map(weekname => {
+        return {name: weekname}
+      })
+      const formattedCumulative = weeks.map(weekname => {
+        return {name: weekname}
+      })
+
+      const pointData = dataService.getAllPoints()
+      
+      pointData.forEach(student => {
+        student.points.forEach(pointObject => {
+          let keyname = Object.keys(pointObject)[0]
+          formatted[keyname -1][student.id] = pointObject[keyname]
+        })
+        student.cumulativePoints.forEach(pointObject => {
+          let keyname = Object.keys(pointObject)[0]
+          formattedCumulative[keyname -1][student.id] = pointObject[keyname]
+        })
+      });
+      setWeeklyPoints(formatted)
+      setcumulativeWeeklyPoints(formattedCumulative)
+
+      setWeeksToShow(weeks)
+    }, []
+  )
+
+  const filterWeeks = (formattedData) => {
+    return formattedData.filter(week => {
+      return weeksToShow.includes(week.name)
+    })
+  }
+
   return (
-    <div>Progress tab!</div>
+    <>
+      <LineChartVisu data={filterWeeks(weeklyPoints)}
+                     title={'Weekly Points'}
+                     keys={studentIds}
+                     axisNames={['Week', ''/*'Points'*/]}>
+      </LineChartVisu>
+      
+      <LineChartVisu data={filterWeeks(cumulativeWeeklyPoints)}
+                     title={'Cumulative Weekly Points'}
+                     keys={studentIds}
+                     axisNames={['Week', ''/*'Points'*/]}>
+      </LineChartVisu>
+    </>
   )
 }
 
